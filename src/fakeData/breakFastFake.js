e0731a62fdf3013035476924693e1fc1ff32facf
BK Uddin <1bkhossain@gmail.com>
2021-10-14 17:18:27 +0600

    Defined all Fake Data

---
+++ src/fakeData/breakFastFake.js
@@ -0,0 +1,55 @@
+const breakfast = [
+  {
+    id: 1,
+    name: "Bagel and cream cheese",
+    details:
+      "A bagel and cream cheese is a common food pairing in American cuisine",
+    price: 6.99,
+    img: "https://i.ibb.co/GCXj543/breakfast1.png",
+  },
+
+  {
+    id: 2,
+    name: "Breakfast sandwich",
+    details:
+      "A breakfast sandwich is any sandwich filled with foods associated with the breakfast meal. ",
+    price: 9.99,
+    img: "https://i.ibb.co/QpxNp29/breakfast2.png",
+  },
+
+  {
+    id: 3,
+    name: "Baked Chicken",
+    details:
+      "Today it's all about the oven so if you're ready to learn how to bake chicken legs with deliciously crispy skin keep on scrollin'!",
+    price: 10.99,
+    img: "https://i.ibb.co/ZYR6Tjh/breakfast3.png",
+  },
+
+  {
+    id: 4,
+    name: "Eggs Benedict",
+    details:
+      "Eggs Benedict is a common American breakfast or brunch dish, consisting of two halves of an English muffin, each topped with Canadian bacon, a poached egg, and hollandaise sauce.",
+    price: 8.99,
+    img: "https://i.ibb.co/zPxJq3X/breakfast4.png",
+  },
+
+  {
+    id: 5,
+    name: "Toast Croissant Fried egg",
+    details:
+      "It includes soft scrambled eggs, veggies, and cheddar cheese! ... Add your eggs (1 1/2 eggs per sandwich) to the toasted croissant bottoms",
+    price: 18.99,
+    img: "https://i.ibb.co/5njRPjK/breakfast5.png",
+  },
+
+  {
+    id: 6,
+    name: "Full Breakfast Fried Egg Toast Brunch",
+    details:
+      "Full English breakfasts contain: sausages, back bacon, eggs, tomatoes, mushrooms, ... The bread has to be FRIED, either with butter or oil.",
+    price: 3.99,
+    img: "https://i.ibb.co/0yZ6Rx6/breakfast6.png",
+  },
+];
---
+++ src/fakeData/lunchFake.js
@@ -0,0 +1,50 @@
+const lunch = [
+  {
+    id: 1,
+    name: "Healthy Meal Plan",
+    details:
+      'At Healthy Meal Plans, we understand that "healthy" actually means something different to everyone.',
+    price: 23.99,
+    img: "https://i.ibb.co/TTNLbC5/lunch1.png",
+  },
+  {
+    id: 2,
+    name: "Fried Chicken Bento",
+    details:
+      "Bento Box with Karaage as a main is called Karaage Bento. It consists of cooked rice, karaage (Japanese fried chicken)",
+    price: 9.99,
+    img: "https://i.ibb.co/5WDMxM7/lunch2.png",
+  },
+  {
+    id: 3,
+    name: "Tarragon-Rubbed-Salmon",
+    details:
+      "In small bowl, combine tarragon, 1/4 teaspoon salt, and 1/4 teaspoon coarsely ground black pepper. Brush salmon with oil and rub with tarragon",
+    price: 6.99,
+    img: "https://i.ibb.co/r5SP3zD/lunch3.png",
+  },
+  {
+    id: 4,
+    name: "Indian Lunch",
+    details:
+      "Lunch recipes - A collection of over 205 vegetarian, healthy, delicious & easy Indian lunch box recipes like curry, parathas, pulaos, biryanis, fried rice",
+    price: 8.99,
+    img: "https://i.ibb.co/SrqyspZ/lunch4.png",
+  },
+  {
+    id: 5,
+    name: "Beef Steak",
+    details:
+      "A beefsteak, often called just steak, is a flat cut of beef with parallel faces, usually cut perpendicular to the muscle fibers. ",
+    price: 15.99,
+    img: "https://i.ibb.co/GPhnWH2/lunch5.png",
+  },
+  {
+    id: 6,
+    name: "Honey-Soy-Glazed Salmon with Peppers",
+    details:
+      "Honey Soy Glazed Salmon - Grilled, pan seared, or baked fillets are topped with a flavorful glaze you'll want to eat on repeat. ",
+    price: 7.99,
+    img: "https://i.ibb.co/6NJYCYR/lunch6.png",
+  },
+];
